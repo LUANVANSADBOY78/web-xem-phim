9009a53510f0f231169a4962565840075c2fd67d
LUANVANSADBOY78 <LUANVANSADBOY78@users.noreply.github.com>
2026-08-28 17:36:00 +0700

    fix: use HashRouter for GitHub Pages support

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import 'swiper/swiper.min.css';
 import './assets/boxicons-2.0.7/css/boxicons.min.css';
 import './App.scss';
 
-import {BrowserRouter, Route} from 'react-router-dom';
+import {HashRouter, Route} from 'react-router-dom';
+
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Chatbot from './components/chatbot/Chatbot';
+
 import Routes from './config/Routes';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Route render={props => (
         <>
-          <Header {...props} />
+          <Header {...props}/>
           <Routes/>
+          <Chatbot/>
           <Footer/>
-          <Chatbot />
         </>
       )}/>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
